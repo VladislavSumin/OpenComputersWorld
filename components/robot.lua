816cceb2c447e7ddf7afb7c0fe7fdbf4867a3ddf
vladislavsumin <vladislavsumin@mail.ru>
2020-04-22 19:09:42 +0300

    update components doc

--- components/robot.lua
+++ components/robot.lua
@@ -1,210 +1,183 @@
 local robot = {}
 
--- Compare the block on the specified side with the one in the selected slot. Returns true if equal.
+--- Compare the block on the specified side with the one in the selected slot. Returns true if equal.
+--- @param side number
+--- @param fuzzy boolean=false
+--- @return boolean
 function robot.compare(side, fuzzy)
-	-- side:number
-	-- fuzzy:boolean=false
-
-	-- return boolean 
 end
 
--- Compare the fluid in the selected tank with the fluid in the specified tank on the specified side. Returns true if equal.
+--- Compare the fluid in the selected tank with the fluid in the specified tank on the specified side. Returns true if equal.
+--- @param side number
+--- @param tank number
+--- @return boolean
 function robot.compareFluid(side, tank)
-	-- side:number
-	-- tank:number
-
-	-- return boolean 
 end
 
--- Compares the fluids in the selected and the specified tank. Returns true if equal.
+--- Compares the fluids in the selected and the specified tank. Returns true if equal.
+--- @param index number
+--- @return boolean
 function robot.compareFluidTo(index)
-	-- index:number
-
-	-- return boolean 
 end
 
--- Compare the contents of the selected slot to the contents of the specified slot.
+--- Compare the contents of the selected slot to the contents of the specified slot.
+--- @param otherSlot number
+--- @param checkNBT boolean=false
+--- @return boolean
 function robot.compareTo(otherSlot, checkNBT)
-	-- otherSlot:number
-	-- checkNBT:boolean=false
-
-	-- return boolean 
 end
 
--- Get the number of items in the specified slot, otherwise in the selected slot.
+--- Get the number of items in the specified slot, otherwise in the selected slot.
+--- @param slot number
+--- @return number
 function robot.count(slot)
-	-- slot:number
-
-	-- return number 
 end
 
--- Checks the contents of the block on the specified sides and returns the findings.
+--- Checks the contents of the block on the specified sides and @returns the findings.
+--- @param side number
+--- @return boolean, string
 function robot.detect(side)
-	-- side:number
-
-	-- return boolean, string 
 end
 
--- Drains the specified amount of fluid from the specified side. Returns the amount drained, or an error message.
+--- Drains the specified amount of fluid from the specified side. Returns the amount drained, or an error message.
+--- @param side boolean
+--- @param amount number=1000
+--- @return boolean, number or string
 function robot.drain(side, amount)
-	-- side:boolean
-	-- amount:number=1000
-
-	-- return boolean, number or string 
 end
 
--- Drops items from the selected slot towards the specified side.
+--- Drops items from the selected slot towards the specified side.
+--- @param side number
+--- @param count number=64
+--- @return boolean
 function robot.drop(side, count)
-	-- side:number
-	-- count:number=64
-
-	-- return boolean 
 end
 
--- Get the durability of the currently equipped tool.
+--- Get the durability of the currently equipped tool.
+--- @return number
 function robot.durability()
 
-	-- return number 
 end
 
--- Eject the specified amount of fluid to the specified side. Returns the amount ejected or an error message.
+--- Eject the specified amount of fluid to the specified side. Returns the amount ejected or an error message.
+    --- @param side number
+    --- @param amount number=1000
+    --- @return boolean, number of string
 function robot.fill(side, amount)
-	-- side:number
-	-- amount:number=1000
-
-	-- return boolean, number of string 
 end
 
--- Get the current color of the activity light as an integer encoded RGB value (0xRRGGBB).
+--- Get the current color of the activity light as an integer encoded RGB value (0xRRGGBB).
+    --- @return number
 function robot.getLightColor()
-
-	-- return number 
 end
 
--- The size of this device's internal inventory.
+--- The size of this device's internal inventory.
+    --- @return number
 function robot.inventorySize()
-
-	-- return number 
 end
 
--- Move in the specified direction.
+--- Move in the specified direction.
+--- @param direction number
+--- @return boolean
 function robot.move(direction)
-	-- direction:number
-
-	-- return boolean 
 end
 
--- Get the name of the agent.
+--- Get the name of the agent.
+    --- @return string
 function robot.name()
-
-	-- return string 
 end
 
--- Place a block towards the specified side. The `face' allows a more precise click calibration, and is relative to the targeted blockspace.
+--- Place a block towards the specified side. The `face' allows a more precise click calibration, and is relative to the targeted blockspace.
+    --- @param side number
+    --- @param face number=side
+    --- @param sneaky boolean=false
+    --- @return boolean
 function robot.place(side, face, sneaky)
-	-- side:number
-	-- face:number=side
-	-- sneaky:boolean=false
-
-	-- return boolean 
 end
 
--- Get the currently selected slot; set the selected slot if specified.
+--- Get the currently selected slot; set the selected slot if specified.
+    --- @param slot number
+    --- @return number
 function robot.select(slot)
-	-- slot:number
-
-	-- return number 
 end
 
--- Select a tank and/or get the number of the currently selected tank.
+--- Select a tank and/or get the number of the currently selected tank.
+    --- @param index number
+    --- @return number
 function robot.selectTank(index)
-	-- index:number
-
-	-- return number 
 end
 
--- Set the color of the activity light to the specified integer encoded RGB value (0xRRGGBB).
+--- Set the color of the activity light to the specified integer encoded RGB value (0xRRGGBB).
+    --- @param value number
+    --- @return number
 function robot.setLightColor(value)
-	-- value:number
-
-	-- return number 
 end
 
--- Get the remaining space in the specified slot, otherwise in the selected slot.
+--- Get the remaining space in the specified slot, otherwise in the selected slot.
+    --- @param slot number
+    --- @return number
 function robot.space(slot)
-	-- slot:number
-
-	-- return number 
 end
 
--- Suck up items from the specified side.
+--- Suck up items from the specified side.
+    --- @param side number
+    --- @param count number=64
+    --- @return boolean
 function robot.suck(side, count)
-	-- side:number
-	-- count:number=64
-
-	-- return boolean 
 end
 
--- Perform a 'left click' towards the specified side. The `face' allows a more precise click calibration, and is relative to the targeted blockspace.
+--- Perform a 'left click' towards the specified side. The `face' allows a more precise click calibration, and is relative to the targeted blockspace.
+    --- @param side number
+    --- @param face number=side
+    --- @param sneaky boolean=false
+    --- @return boolean, string
 function robot.swing(side, face, sneaky)
-	-- side:number
-	-- face:number=side
-	-- sneaky:boolean=false
-
-	-- return boolean, string 
 end
 
--- The number of tanks installed in the device.
+--- The number of tanks installed in the device.
+    --- @return number
 function robot.tankCount()
-
-	-- return number 
 end
 
--- Get the fluid amount in the specified or selected tank.
+--- Get the fluid amount in the specified or selected tank.
+    --- @param index number
+    --- @return number
 function robot.tankLevel(index)
-	-- index:number
-
-	-- return number 
 end
 
--- Get the remaining fluid capacity in the specified or selected tank.
+--- Get the remaining fluid capacity in the specified or selected tank.
+    --- @param index number
+    --- @return number
 function robot.tankSpace(index)
-	-- index:number
-
-	-- return number 
 end
 
--- Move the specified amount of fluid from the selected tank into the specified tank.
+--- Move the specified amount of fluid from the selected tank into the specified tank.
+    --- @param index number
+    --- @param count number=1000
+    --- @return boolean
 function robot.transferFluidTo(index, count)
-	-- index:number
-	-- count:number=1000
-
-	-- return boolean 
 end
 
--- Move up to the specified amount of items from the selected slot into the specified slot.
+--- Move up to the specified amount of items from the selected slot into the specified slot.
+    --- @param toSlot number
+    --- @param amount number
+    --- @return boolean
 function robot.transferTo(toSlot, amount)
-	-- toSlot:number
-	-- amount:number
-
-	-- return boolean 
 end
 
--- Rotate in the specified direction.
+--- Rotate in the specified direction.
+--- @param clockwise boolean
+--- @return boolean
 function robot.turn(clockwise)
-	-- clockwise:boolean
-
-	-- return boolean 
 end
 
--- Perform a 'right click' towards the specified side. The `face' allows a more precise click calibration, and is relative to the targeted blockspace.
+--- Perform a 'right click' towards the specified side. The `face' allows a more precise click calibration, and is relative to the targeted blockspace.
+    --- @param side number
+    --- @param face number=side
+    --- @param sneaky boolean=false
+    --- @param duration number=0
+    --- @return boolean, string
 function robot.use(side, face, sneaky, duration)
-	-- side:number
-	-- face:number=side
-	-- sneaky:boolean=false
-	-- duration:number=0
-
-	-- return boolean, string 
 end
 
 return robot
--- components/tractor_beam.lua
+++ components/tractor_beam.lua
@@ -1,9 +1,8 @@
 local tractor_beam = {}
 
--- Tries to pick up a random item in the robots' vicinity.
+--- Tries to pick up a random item in the robots' vicinity.
+--- @return boolean
 function tractor_beam.suck()
-
-	-- return boolean 
 end
 
 return tractor_beam
